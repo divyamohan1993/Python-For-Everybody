d0d84b3cfa81937a6f7b17fb6a13a4a972e54a77
Divya Mohan <divyamohan1993@users.noreply.github.com>
2024-07-22 15:55:23 +0000

    reads the span comments, gets the number and adds it.

--- Scraping HTML Data with BeautifulSoup/solution.py
+++ Scraping HTML Data with BeautifulSoup/solution.py
@@ -11,15 +11,19 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
+# url = input('Enter - ')
+# url = "http://py4e-data.dr-chuck.net/comments_42.html"
+url = "http://py4e-data.dr-chuck.net/comments_2003850.html"
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
-tags = soup('a')
+tags = soup('span')
+sum = 0
 for tag in tags:
     # Look at the parts of a tag
-    print('TAG:', tag)
-    print('URL:', tag.get('href', None))
-    print('Contents:', tag.contents[0])
-    print('Attrs:', tag.attrs)
+    # print('TAG:', tag)    
+    # print('Contents:', tag.contents[0])
+    sum += int(tag.contents[0])
+    
+print(sum)
