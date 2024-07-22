9de6e31d0f8a40300d1939d65b8bc1039acddfd9
Divya Mohan <divyamohan1993@users.noreply.github.com>
2024-07-22 17:21:03 +0000

    get name from file without recursion

--- Following Links in Python/solution.py
+++ Following Links in Python/solution.py
@@ -1,3 +1,10 @@
+# Time Taken: 57 minutes
+# Tries: 58
+
+# I AM A NOOB :)
+
+# can also be done with recursion. 
+
 # To run this, download the BeautifulSoup zip file
 # http://www.py4e.com/code3/bs4.zip
 # and unzip it in the same directory as this file
@@ -11,11 +18,39 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
+# url = input('Enter - ')
+url = "http://py4e-data.dr-chuck.net/known_by_Fikret.html" # sample url
+url = "http://py4e-data.dr-chuck.net/known_by_Ayren.html" # actual url
 
 # Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+def get_link(url):    
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    return tags
+    # tags = soup('a')
+    # return tags[position].get('href', None)
+
+# html = urllib.request.urlopen(url, context=ctx).read()
+# soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+# print (get_link(url, 2).contents[0])
+#tags = soup('a')
+
+link_position = int(input("Enter position: ")) - 1
+repeats = int(input("Enter repeat count: "))
+
+# for loop
+for _ in range(repeats):    
+    tags = get_link(url)[link_position]
+    print("Retrieving: ", tags.get('href', None))
+    # print(tags.contents[0])
+    url = tags.get('href', None)
+
+print(tags.contents[0])
+
+# newlink = tags.get('href', None)
+# newtags = get_link(newlink)
+
+# print(get_link(url, link_position).contents[0])
